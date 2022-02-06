02c74b5f0a8a9bbf5995a1aa09f2095a51776073
Bernardo Ramos <berna.gensis@gmail.com>
2022-02-06 03:02:03 -0300

    add burn

--- src/ARC2-Core.lua
+++ src/ARC2-Core.lua
@@ -167,5 +167,16 @@ function transfer(to, tokenId, ...)
 end
 
 
-abi.register(transfer)
+function burn(tokenId)
+  _typecheck(tokenId, 'str128')
+
+  local owner = ownerOf(tokenId)
+  assert(owner ~= nil, "ARC2: burn - nonexisting token")
+  assert(system.getSender() == owner, "ARC2: cannot burn a token that is not own")
+
+  _burn(tokenId)
+end
+
+
+abi.register(transfer, burn)
 abi.register_view(name, symbol, balanceOf, ownerOf)
