630fb2df00f4d55f5baea15e1a9ad91e53f586ea
Bernardo Ramos <berna.gensis@gmail.com>
2022-02-08 16:21:13 -0300

    add totalSupply, nextToken, findToken

--- src/ARC2-Core.lua
+++ src/ARC2-Core.lua
@@ -17,7 +17,12 @@ local function _typecheck(x, t)
   elseif (x and t == 'str128') then
     assert(type(x) == 'string', "str128 must be string type")
     -- check address length
-    assert(128 >= #x, string.format("too long str128 length: %s", #x))
+    assert(#x <= 128, string.format("too long str128 length: %s", #x))
+  elseif (x and t == 'uint') then
+    -- check unsigned integer
+    assert(type(x) == 'number', string.format("invalid type: %s != number", type(x)))
+    assert(math.floor(x) == x, "the number must be an integer")
+    assert(x >= 0, "the number must be 0 or positive")
   else
     -- check default lua types
     assert(type(x) == t, string.format("invalid type: %s != %s", type(x), t or 'nil'))
@@ -31,6 +36,9 @@ state.var {
   _name = state.value(),            -- string
   _symbol = state.value(),          -- string
 
+  _num_burned = state.value(),      -- integer
+  _last_index = state.value(),      -- integer
+  _ids = state.map(),               -- integer -> str128
   _tokens = state.map(),            -- str128 -> { owner: address, approved: address }
   _balances = state.map(),          -- address -> unsigned_bignum
 
@@ -49,6 +57,9 @@ local function _init(name, symbol)
   
   _name:set(name)
   _symbol:set(symbol)
+
+  _last_index:set(0)
+  _num_burned:set(0)
 
   _paused:set(false)
 end
@@ -109,6 +120,10 @@ local function _mint(to, tokenId, ...)
 
   assert(not _exists(tokenId), "ARC2: mint - already minted token")
 
+  local index = _last_index:get() + 1
+  _last_index:set(index)
+  _ids[tostring(index)] = tokenId
+
   local token = {
     owner = to
   }
@@ -130,8 +145,15 @@ local function _burn(tokenId)
   assert(not _paused:get(), "ARC2: paused contract")
   assert(not _blacklist[owner], "ARC2: owner is on blacklist")
 
+  local index,_ = findToken({contains=tokenId}, 0)
+  assert(index ~= nil and index > 0, "burn: token not found")
+  -- _ids[tostring(index)] = nil
+  _ids:delete(tostring(index))
+
   _tokens:delete(tokenId)
   _balances[owner] = _balances[owner] - 1
+
+  _num_burned:set(_num_burned:get() + 1)
 
   contract.event("burn", owner, tokenId)
 end
@@ -195,5 +217,102 @@ function burn(tokenId)
 end
 
 
+function totalSupply()
+  return _last_index:get() - _num_burned:get()
+end
+
+function nextToken(prev_index)
+  _typecheck(prev_index, 'uint')
+
+  local index = prev_index
+  local last_index = _last_index:get()
+  local tokenId
+
+  if index >= last_index then
+    return nil, nil
+  end
+
+  do
+    index = index + 1
+    tokenId = _ids[tostring(index)]
+  while tokenId == nil and index < last_index
+
+  if index == last_index and tokenId == nil then
+    index = nil
+  end
+
+  return index, tokenId
+end
+
+-- retrieve the first token found that mathes the query
+-- the query is a table that can contain these fields:
+--   owner    - the owner of the token (address)
+--   contains - check if the tokenId contains this string
+--   pattern  - check if the tokenId matches this Lua regex pattern
+-- the prev_index must be 0 in the first call
+-- for the next calls, just inform the returned index from the last call
+-- return value: 2 variables: index and tokenId
+-- if no token is found with the given query, it returns (nil, nil)
+function findToken(query, prev_index)
+  _typecheck(query, 'table')
+  _typecheck(prev_index, 'uint')
+
+  local contains = query["contains"]
+  if contains then
+    query["pattern"] = escape(contains)
+  end
+
+  local index = prev_index
+  local last_index = _last_index:get()
+  local tokenId, owner
+
+  if index >= last_index then
+    return nil, nil
+  end
+
+  do
+    index = index + 1
+    tokenId = _ids[tostring(index)]
+    if not token_matches(tokenId, query) then
+      tokenId = nil
+    end
+  while tokenId == nil and index < last_index
+
+  if index == last_index and tokenId == nil then
+    index = nil
+  end
+
+  return index, tokenId
+end
+
+local function token_matches(tokenId, query)
+
+  if tokenId == nil then
+    return false
+  end
+
+  local user = query["owner"]
+  local pattern = query["pattern"]
+
+  if user then
+    local owner = ownerOf(tokenId)
+    if owner ~= user then
+      return false
+    end
+  end
+
+  if pattern then
+    if not tokenId:match(pattern) then
+      return false
+    end
+  end
+
+  return true
+end
+
+local function escape(str)
+  return str:gsub("[%(%)%.%%%+%-%*%?%[%]%^%$]", function(c) return "%" .. c end)
+end
+
 abi.register(transfer, burn)
-abi.register_view(name, symbol, balanceOf, ownerOf)
+abi.register_view(name, symbol, balanceOf, ownerOf, totalSupply, nextToken, findToken)
