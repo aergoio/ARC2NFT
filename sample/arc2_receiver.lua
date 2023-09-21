3d62ba036516b23ab12154414f3ebac970be2cc9
Bernardo Ramos <bernardo@blocko.io>
2023-09-20 22:31:50 -0300

    fix example of ARC2 receiver contract

--- sample/arc2_receiver.lua
+++ sample/arc2_receiver.lua
@@ -1,12 +1,24 @@
+state.var {
+  accepted_tokens = state.map()
+}
 
--- ARC2 token receiver interface
+-- This function is called when an ARC2 token is sent to this contract
+-- But it is also called by any account, so do not trust it without making checks first
 function nonFungibleReceived(operator, from, tokenId, ...)
+  local token_contract = system.getSender()
+  assert(accepted_tokens[token_contract] == true, "token not supported")
+
   contract.event("GotARC2", operator, from, tokenId, ...)
-  return "GotARC2: " .. tokenId
+end
+
+function add_accepted_token(address)
+  assert(system.getSender() = system.getCreator(), "permission denied")
+  accepted_tokens[address] = true
 end
 
 function transferARC2(contractId, to, tokenId, ...)
+  assert(system.getSender() = system.getCreator(), "permission denied")
   contract.call(contractId, "transfer", to, tokenId, ...)
 end
 
-abi.register(transferARC2, nonFungibleReceived)
+abi.register(nonFungibleReceived, add_accepted_token, transferARC2)
